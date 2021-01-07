8a606d669d0943cda22a06793bb7f11109035494
wickstjo <jfwick@gmail.com>
2021-01-07 18:55:37 +0200

    Rewrote migration script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,13 +1,11 @@
-# REMOVE THE OLD BUILD DIR
+# NUKE OLD BUILD
 rm -rf build/
 
-# MIGRATE CONTRACTS TO THE BLOCKCHAIN
+# MIGRATE SMART CONTRACTS
 truffle migrate --network development
 
-# COPY REFERENCES TO THE OTHER PROJECTS
-node ./scripts/transfer.js
+# DISTRIBUTE UNIFIED ABI FILE TO OTHER REPOS
+python3 scripts/migrate.py
 
-# CLEAN UP GARBAGE
+# CLEAN UP
 rm -rf build/
-rm -rf bin/
-rm -rf contracts/bin/
